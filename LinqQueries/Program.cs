b13dfd424af83ef4216c32137743aebcaac545dc
Tarun sharma <tarunsharma.9097@gmail.com>
2024-03-28 16:49:00 +0530

    Add project files.

---
+++ LinqQueries/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace LinqQueries
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
